57c5379a97a6241b8f9aaf49e0b6b7fcf6a5a2fd
Sherri Ase <shirin.asenova90@gmail.com>
2023-06-17 18:50:18 +0300

    initial commit for Hotel

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Please select: \n1 - Make a reservation; \n2 - List free rooms; \n3 - Checkout room; \n4 - Stats; \n5 - Find a room; \n6 - Update a room");
+    }
+}
